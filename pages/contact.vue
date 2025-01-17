26485567d2084235a7a2c9e24523385736ab3828
ManUtopiK <emmanuel.salomon@gmail.com>
2025-01-17 15:02:25 +0100

    Use shadcn form components and validate with zod

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,25 +1,53 @@
 <template>
     <section class="relative flex flex-col items-center justify-center min-h-screen bg-gray-100">
-        <Card v-if="!showAlert">
+        <Card v-if="!showAlert" class="w-1/2">
             <CardHeader>
                 <CardTitle class="text-2xl font-bold text-center text-gray-800 mb-4">Contactez-nous</CardTitle>
             </CardHeader>
+
             <CardContent>
-                <form @submit.prevent="sendMail">
-                    <div class="mb-4">
-                        <Label for="sujet" class="block text-sm font-medium text-gray-700 mb-2">Sujet</Label>
-                        <Input id="sujet" v-model="sujet" type="text"
-                            class="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
-                            placeholder="Entrez le sujet" required />
-                    </div>
-                    <div class="mb-4">
-                        <Label for="message" class="block text-sm font-medium text-gray-700 mb-2">Message</Label>
-                        <Textarea id="message" v-model="message" rows="4"
-                            class="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
-                            placeholder="Entrez votre message" required></Textarea>
-                    </div>
-                    <Button type="submit"
-                        class="w-full py-2 px-4 text-white bg-indigo-600 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <form @submit.prevent="sendMail" class="gap-4 flex flex-col">
+                    <FormField v-slot="{ componentField }" name="username">
+                        <FormItem>
+                            <FormLabel>Votre nom</FormLabel>
+                            <FormControl>
+                                <Input type="text" placeholder="Entrez votre nom" v-bind="componentField" />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    </FormField>
+
+                    <FormField v-slot="{ componentField }" name="email">
+                        <FormItem>
+                            <FormLabel>Votre email</FormLabel>
+                            <FormControl>
+                                <Input type="text" placeholder="Entrez votre email" v-bind="componentField" />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    </FormField>
+
+                    <FormField v-slot="{ componentField }" name="sujet">
+                        <FormItem>
+                            <FormLabel>Le sujet</FormLabel>
+                            <FormControl>
+                                <Input type="text" placeholder="Entrez le sujet" v-bind="componentField" />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    </FormField>
+
+                    <FormField v-slot="{ componentField }" name="message">
+                        <FormItem>
+                            <FormLabel>Votre message</FormLabel>
+                            <FormControl>
+                                <Textarea placeholder="Entrez votre message" v-bind="componentField" />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    </FormField>
+
+                    <Button type="submit" class="w-full text-white bg-indigo-600">
                         Envoyer
                     </Button>
                 </form>
@@ -32,13 +60,13 @@
                 <Alert>
                     <AlertTitle>Message envoyé avec succès</AlertTitle>
                     <AlertDescription>
-                        <p>Sujet : {{ sujet }}</p>
-                        <p>Message : {{ message }}</p>
+                        <p>Nom : {{ payload.username }}</p>
+                        <p>Email : {{ payload.email }}</p>
+                        <p>Sujet : {{ payload.sujet }}</p>
+                        <p>Message : {{ payload.message }}</p>
                     </AlertDescription>
                 </Alert>
-                <Button
-                    class="mt-4 w-full py-2 px-4 text-white bg-indigo-600 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    @click="closeAlert">
+                <Button class="mt-4 w-full py-2 px-4 text-white bg-indigo-600 rounded-md" @click="closeAlert">
                     OK
                 </Button>
             </Card>
@@ -46,58 +74,46 @@
     </section>
 </template>
 
----
+<script setup lang="ts">
+import { useForm } from 'vee-validate'
+import { toTypedSchema } from '@vee-validate/zod'
+import * as z from 'zod'
 
-### Script
+const formSchema = toTypedSchema(z.object({
+    username: z.string().min(2).max(50),
+    email: z.string().email(),
+    sujet: z.string().min(2),
+    message: z.string().min(2),
+}))
 
-```javascript
-<script>
-import { Button } from '@/components/ui/button';
-import { Label } from '@/components/ui/label';
-import { Input } from '@/components/ui/input';
-import { Textarea } from '@/components/ui/textarea';
-import {
-    Card,
-    CardContent,
-    CardHeader,
-    CardTitle,
-} from '@/components/ui/card';
-import { Alert, AlertTitle, AlertDescription } from '@/components/ui/alert';
+const form = useForm({
+    validationSchema: formSchema,
+})
 
-export default {
-    data() {
-        return {
-            sujet: '',
-            message: '',
-            showAlert: false,
-        };
-    },
-    components: {
-        Card,
-        CardContent,
-        CardHeader,
-        CardTitle,
-        Button,
-        Label,
-        Input,
-        Textarea,
-        Alert,
-        AlertTitle,
-        AlertDescription,
-    },
-    methods: {
-        sendMail() {
-            // Show the alert overlay
-            this.showAlert = true;
-        },
-        closeAlert() {
-            // Hide the alert overlay
-            this.showAlert = false;
+const payload = reactive({
+    username: "",
+    email: "",
+    sujet: "",
+    message: "",
+})
+const showAlert = ref(false)
 
-            // Reset form fields after dismissing the alert
-            this.sujet = '';
-            this.message = '';
-        },
-    },
-};
+const sendMail = form.handleSubmit((values) => {
+    Object.assign(payload, values)
+    // Show the alert overlay
+    showAlert.value = true
+})
+
+function closeAlert() {
+    // Hide the alert overlay
+    showAlert.value = false
+
+    // Reset form fields after dismissing the alert
+    Object.assign(payload, {
+        username: "",
+        email: "",
+        sujet: "",
+        message: "",
+    })
+}
 </script>
